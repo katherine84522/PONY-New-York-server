a9889386154ceb7d12e80d059caac584434581bd
colterlevi <cllongshore@gmail.com>
2023-01-18 09:17:15 -0500

    websockets connected on backend

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from config import Config
 from models import db, Protector, Walkee, Requests
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__, static_folder='public')
 CORS(app, origins=['*'])
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 
 @app.get('/')
@@ -155,5 +157,27 @@ def create_requests():
     return jsonify(reqs.to_dict()), 201
 
 
+@socketio.on('connect')
+def connected():
+    '''This function is an event listener that gets called when the client connects to the server'''
+    print(f'Client {request.sid} has connected')
+    emit('connect', {'data': f'id: {request.sid} is connected'})
+
+
+@socketio.on('data')
+def handle_message(data):
+    '''This function runs whenever a client sends a socket message to be broadcast'''
+    print(f'Message from Client {request.sid} : ', data)
+    emit('data', {'data': 'data', 'id': request.sid}, broadcast=True)
+
+
+@socketio.on("disconnect")
+def disconnected():
+    '''This function is an event listener that gets called when the client disconnects from the server'''
+    print(f'Client {request.sid} has disconnected')
+    emit('disconnect',
+         f'Client {request.sid} has disconnected', broadcast=True)
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=os.environ.get('PORT', 3000))
+    socketio.run(app, host='0.0.0.0', port=os.environ.get('PORT', 3000))
