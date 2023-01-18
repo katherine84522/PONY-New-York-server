6e358f26449feff9a79bbcd0211367d25f00edeb
Cooper Hall <cooperhall111@gmail.com>
2023-01-18 15:31:36 -0500

    added seeds and associations (hopefully)

--- seeds.py
+++ seeds.py
@@ -1,7 +1,22 @@
 from app import app
-from models import db, User
+from models import db, Protector, Walkee, Requests
 
-print('Seeding database ... 🌱')
-# Add your seed data here
+def run_seeds():
+  print('Seeding database ... 🌱')
+# Add your seed data 
+  with app.app_context():
+    protector1 = Protector('coop', 'bronze', 'linke@exam.co')
+    protector2 = Protector('jeff', 'bezzos', 'amazo.com')
+    walkee1 = Walkee('kerry', 'nradshaw', 'kbrad@eee.co')
+    walkee2 = Walkee('jim', 'gaffigan', 'jimg@ll.co')
+    request1 = Requests('statue lib', 'my house', '1/14', '2pm', 'message1', 'False', 'False', 'False', 1, 2)
+    request2 = Requests('penn st', 'nyse', '2/11', '5pm',
+                        'message2', 'False', 'False', 'True', 2, 2)
+    db.session.add_all([protector1, protector2, walkee1, walkee2, request1, request2])
+    db.seesion.commit()
+    print('Done! 🌳')
 
-print('Done! 🌳')
+run_seeds
+
+
+
